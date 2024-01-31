bccc54eac47e8e11ab89328eda63a512e12893c8
Riccardo <giordanobruno227@gmail.com>
2024-02-01 00:27:46 +0100

    navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button'; 
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatSidenavModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {
-  
+export class NavbarComponent implements OnInit {
+
+  isNavExpanded = true;
+
+  ngOnInit() {
+    if (window.innerWidth <= 768) {
+      this.isNavExpanded = false;
+    }
+  }
+
+  toggleNav() {
+    this.isNavExpanded = !this.isNavExpanded;
+  }
+
   openSection(sectionId: string){
     const elem = document.getElementById(sectionId);
     if(elem){
