f09b49c4581f57f9cd568a4c65da1d575add1775
Riccardo <giordanobruno227@gmail.com>
2024-02-03 01:21:16 +0100

    try ùform

--- src/app/components/contactform/contactform.component.ts
+++ src/app/components/contactform/contactform.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-contactform',
@@ -11,6 +11,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
   styleUrl: './contactform.component.css'
 })
 export class ContactformComponent {
+
   profileForm = new FormGroup({
     email: new FormControl(''),
     object: new FormControl(''),
@@ -21,26 +22,30 @@ export class ContactformComponent {
 
   onSubmit() {
 
-    console.warn(this.profileForm.value);
+    console.log(this.profileForm.value);
 
-    const email = this.profileForm.get('email')?.value as string;
-    const object = this.profileForm.get('object')?.value as string;
-    const message = this.profileForm.get('message')?.value as string;
+    const formData: any = this.profileForm.value;
 
-    // Check if values are defined before using them
-    if (email !== undefined && object !== undefined && message !== undefined) {
-      // Prepare the data for the POST request
-      const formData = new HttpParams()
-        .set('email', email)
-        .set('object', object)
-        .set('message', message);
+    formData['form-name'] = 'contact';
+
+    const headers = new HttpHeaders({
+      Accept: 'text/html',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    });
+
+    this.http
+      .post('/', new URLSearchParams(formData).toString(), { headers, responseType: 'text' })
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 
       // Make the POST request to Netlify
-      this.http.post('/', formData.toString(), {
+      /*this.http.post('/', formData.toString(), {
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
       }).subscribe(response => {
         console.log(response);
       });
     }
-  }
+  }*/
 }
